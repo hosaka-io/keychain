60e86cea98913098ada7753b658224f6d5786855
Brian Edwards <brian@edwardstx.us>
2018-05-05 11:58:44 -0700

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.hosaka/keychain "0.1.0-SNAPSHOT"
+(defproject io.hosaka/keychain "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
