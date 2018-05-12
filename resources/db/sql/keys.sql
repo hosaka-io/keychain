347401d0d03638326367b54ca644345901d02018
Brian Edwards <brian@edwardstx.us>
2018-05-11 21:42:16 -0700

    Fixed authoritative column

--- resources/db/sql/keys.sql
+++ resources/db/sql/keys.sql
@@ -1,6 +1,6 @@
 -- db/sql/keys.sql
 
 -- :name get-key-sql :? :1
-SELECT kid, key_data, can_add, created_on, algorithm
+SELECT kid, key_data, authoritative, created_on, algorithm
   FROM keychain.keys
  WHERE kid = :kid
